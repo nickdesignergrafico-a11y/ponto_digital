6a118e18c04b51692d656cffecdb6bd48fe3537c
enikersom809 <enikersom809@gmail.com>
2026-08-10 10:04:25 -0300

    fix: atualiza conexao para o banco americano pontodigital

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,23 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore, getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import firebaseConfig from '../../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+
+const databaseId = 'pontodigital';
+
+let firestoreDb;
+try {
+  firestoreDb = initializeFirestore(app, {
+    experimentalForceLongPolling: true,
+  }, databaseId);
+} catch (e) {
+  firestoreDb = getFirestore(app, databaseId);
+}
+
+export const db = firestoreDb;
 export const storage = getStorage(app);
+
